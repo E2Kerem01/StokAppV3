6fad0138a13bf6cd1c322707ec3a857e0e8a3e30
Kerem Metin <k.metin01@gmail.com>
2024-01-05 14:38:26 +0300

    feat: kerem v6

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -135,11 +135,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Örnek olarak bu yol kullanılabilir
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-
+    # Burada, projenizdeki statik dosyaların bulunduğu yerler belirtilebilir
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
